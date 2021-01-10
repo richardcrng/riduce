fbe2fed75555c7bdf70b1f2676a5106ea1bbd16a
richardcrng@gmail.com <richardcrng@gmail.com>
2021-01-10 17:09:45 +0000

    Add type for useRiducer

--- src/use-riducer/useRiducer.ts
+++ src/use-riducer/useRiducer.ts
@@ -1,11 +1,18 @@
-import { useRef, useReducer } from "react";
-import riduce from "../riduce";
-import { RiducerDict } from "../types";
+import { useRef, useReducer, Dispatch } from "react";
+import riduce, { Reducer } from "../riduce";
+import { Action, ActionsProxy, RiducerDict } from "../types";
+
+type UsedRiducer<TreeT, RiducerDictT extends RiducerDict<TreeT>> = {
+  state: TreeT;
+  dispatch: Dispatch<Action>;
+  actions: ActionsProxy<TreeT, TreeT, RiducerDictT>;
+  reducer: Reducer<TreeT, Action>;
+};
 
 function useRiducer<TreeT, RiducerDictT extends RiducerDict<TreeT>>(
   initialState: TreeT,
   riducerDict: RiducerDictT = {} as RiducerDictT
-) {
+): UsedRiducer<TreeT, RiducerDictT> {
   const riduceRef = useRef(riduce(initialState, riducerDict));
   const [reducer, actions] = riduceRef.current;
   const [state, dispatch] = useReducer(reducer, initialState);
